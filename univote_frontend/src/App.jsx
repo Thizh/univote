53f62d389c0e7cc25476cf3e720610870c3597fc
Thizh Wick <s22010229@ousl.lk>
2024-09-19 19:08:43 +0530

    reac router

--- univote_frontend/src/App.jsx
+++ univote_frontend/src/App.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './screens/Home';
 import LoginSignUp from './screens/LoginSignUp';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      try {
+        const isLoggedIn = await localStorage.getItem('isLoggedIn');
+        setIsLoggedIn(isLoggedIn === 'true');
+      } catch (error) {
+        console.error('Error retrieving login status:', error);
+      }
+    };
+
+    checkLoginStatus();
+  }, []);
+
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={isLoggedIn ? <Home /> : <Navigate to="/login" />}
+        />
         <Route path="/login" element={<LoginSignUp />} />
       </Routes>
     </Router>
@@ -15,3 +33,4 @@ function App() {
 }
 
 export default App;
+
