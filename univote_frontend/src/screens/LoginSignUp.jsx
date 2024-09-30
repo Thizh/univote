8e7a8df65e8a9772937c0a6a184b45981dd216a9
Thizh Wick <s22010229@ousl.lk>
2024-09-30 23:57:37 +0530

    login ui

--- univote_frontend/src/screens/LoginSignUp.jsx
+++ univote_frontend/src/screens/LoginSignUp.jsx
@@ -1,64 +1,88 @@
 import React, { useState } from 'react';
 import Logo from '../assets/imgs/logo.png'
 import '../css/login.css';
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
 
 function LoginSignUp() {
-  const [isRightPanelActive, setIsRightPanelActive] = useState(false);
+  const [nic, setNic] = useState('');
+  const [name, setName] = useState('');
+  const [password, setPassword] = useState('');
+  const baseurl = 'http://127.0.0.1:8000';
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+  const [secBtnClicked, setSecBtnClicked] = useState(false);
 
-  const handleSignUpClick = () => {
-    setIsRightPanelActive(true);
-  };
+  const fetchStu = async (event) => {
+    event.preventDefault();
+    let res = await fetch(`${baseurl}/api/student-details`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({ nic }),
+    });
+    const data = await res.json();
+    if (data[0]) {
+      setName(data.name);
+    } else {
+      setError('You are not an OUSL student');
+    }
 
-  const handleSignInClick = () => {
-    setIsRightPanelActive(false);
-  };
+    console.log(data);
+  }
+
+  const checkPass = async (event) => {
+    event.preventDefault();
+    let check_pw = await fetch(`${baseurl}/api/checkuser`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+      },
+      body: JSON.stringify({ nic, reg_no: password }),
+    });
+    const pw_data = await check_pw.json();
+    if (pw_data[0]) {
+      localStorage.setItem('isLoggedIn', 'true');
+      window.location.reload();
+    } else {
+      setError('Your Password is wrong');
+    }
+  }
 
   return (
-    <div className={`container ${isRightPanelActive ? 'right-panel-active' : ''}`} id="container">
-      <div className="form-container sign-up-container">
-        <form action="#">
-          <h1>Create Account</h1>
-          <div className="social-container">
-            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-          </div>
-          <span>or use your Registration number for registration</span>
-          <input type="text" placeholder="Name" />
-          <input type="text" placeholder="Student ID" />
-          <input type="password" placeholder="Password" />
-          <button>Sign Up</button>
-        </form>
-      </div>
-      
+    <div className="container" id="container">
       <div className="form-container sign-in-container">
-        <form action="#">
+        <form onSubmit={name ? checkPass : fetchStu}>
           <img src={Logo} alt="A beautiful scenery" width="30" height="40" />
-          <h1>Sign in</h1>
-          <div className="social-container">
-            <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
-            <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
-            <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
-          </div>
-          <span>or use your account</span>
-          <input type="email" placeholder="Email / Student ID" />
-          <input type="password" placeholder="Password" />
-          <a href="#">Forgot your password?</a>
-          <button>Sign In</button>
+          <h1 className="signin-h1">Sign in</h1>
+            {name ? (
+              <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', marginBottom: 10 }}>
+                  <img src={Logo} alt="A beautiful scenery" width="30" height="40" />
+                  <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start', marginLeft: 10 }}>
+                    <p style={{ color: '#000', fontSize: 15, margin: 0 }}>{name}</p>
+                    <p style={{ color: '#000', fontSize: 10, margin: 0 }}>{nic}</p>
+                  </div>
+                </div>
+                <input type="password" placeholder="Password" style={{ padding: '8px', fontSize: '14px', width: '100%' }} onChange={event => setPassword(event.target.value)}/>
+              </div>
+
+            ) : (
+              <input type="text" placeholder="NIC Number" onChange={event => {setNic(event.target.value), setError('')}}/>
+            )}
+            <p style={{color: 'red'}}>{error}</p>
+            
+            <button style={{marginTop: '10%'}}>Sign In</button>
         </form>
       </div>
       
       <div className="overlay-container">
         <div className="overlay">
-          <div className="overlay-panel overlay-left">
-            <h1>Welcome Back!</h1>
-            <p>To keep connected with us please login with your personal info</p>
-            <button className="ghost" id="signIn" onClick={handleSignInClick}>Sign In</button>
-          </div>
           <div className="overlay-panel overlay-right">
-            <h1>Hello, Friend!</h1>
-            <p>Enter your personal details and start the journey with us</p>
-            <button className="ghost" id="signUp" onClick={handleSignUpClick}>Sign Up</button>
+            <h1>Hello Voter!</h1>
+            <p>If you are a first timer login with your NIC and password for your OUSL Registration Number</p>
           </div>
         </div>
       </div>
