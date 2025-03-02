4413889ed69de7ba28b48bedb397d9cc996e3936
Thizh Wick <s22010229@ousl.lk>
2025-03-02 14:05:11 +0530

    fix

--- Scanner/baseurl.js
+++ Scanner/baseurl.js
@@ -1,3 +1,3 @@
-const baseurl = "http://192.168.43.236:8000"
-//const baseurl = "https://univoteadmin.nexelaris.com"
+// const baseurl = "http://192.168.43.236:8000"
+const baseurl = "https://univoteadmin.nexelaris.com"
 export default baseurl;
