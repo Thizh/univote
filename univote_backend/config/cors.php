efbe107af038c0147bc64c88a72fb19352891644
Thizh Wick <s22010229@ousl.lk>
2025-03-10 18:18:01 +0530

    changes

--- univote_backend/config/cors.php
+++ univote_backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://nexelaris.com'],
+    'allowed_origins' => ['https://univote.nexelaris.com'],
 
     'allowed_origins_patterns' => [],
 
